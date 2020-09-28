80d336b9200ca7495989120dcd069ec05c9554bb
tkim30201 <63823948+tkim30201@users.noreply.github.com>
2020-09-28 04:58:09 -0400

    added questions to index

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
 const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub Username?',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Project description',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Installation instructions',
+        name: 'instructions'
+    },
+    {
+        type: 'input',
+        message: 'Project usage',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Licenses for the Project',
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Any contributors',
+        name: 'contributors'
+    },
+    {
+        type: 'input',
+        message: 'Any tests?',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'e-mail',
+        name: 'email'
+    }
 
 ];
 
